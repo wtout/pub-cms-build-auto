76e2d1f75d6229bd6e7dff1ad9aaece08880bc36
Wassim Tout <watout@cisco.com>
2019-09-04 16:04:50 -0400

    Updated the bash script to remove unnecessary code Updated site.yml with play names that are more accurate

--- Bash/.play.sh
+++ Bash/.play.sh
@@ -232,7 +232,7 @@ function enable_logging() {
 function get_credentials() {
 	if [[ ! -f ${CRVAULT} ]]
 	then
-		ansible-playbook prompts.yml --extra-vars "{VFILE: '${CRVAULT}', $(echo $0 | sed -e 's/.*play_\(.*\)\.sh/\1/'): true}" ${@}
+		ansible-playbook prompts.yml --extra-vars "{VFILE: '${CRVAULT}'}" ${@}
 		GET_CREDS_STATUS=${?}
 		encrypt_vault ${CRVAULT} ${VAULTP} ${BBPASS}
 	fi
