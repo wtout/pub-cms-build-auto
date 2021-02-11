01fc088b2c734f4ad36eadbabba9ba404845bcb1
Wassim Tout <watout@cisco.com>
2021-02-11 09:59:01 -0500

    Updated the bash script to upgrade pip3 for the user only and leave the distribution version at the original version

--- Bash/.play.sh
+++ Bash/.play.sh
@@ -207,19 +207,13 @@ function install_packages() {
 	done
 	if [[ "$(python3 -m pip show pip | grep ^Version | awk -F ': ' '{print $NF}')" == "9.0.3" ]]
 	then
-		if [[ "x${SUDO_PASS}" == "x" ]]
-		then
-			echo
-			SUDO_PASS=$(get_sudopass) || FS=${?}
-			[[ "${FS}" == 1 ]] && echo -e "\n${SUDO_PASS}\n" && exit ${FS}
-		fi
 		printf "\nUpgrading PIP to the latest version ..."
-		sudo -S python3 -m pip install -U pip --quiet --proxy="${PROXY_ADDRESS}" <<< ${SUDO_PASS}
+		python3 -m pip install --user --no-cache-dir --quiet -U pip --proxy="${PROXY_ADDRESS}"
 		[[ ${?} == 0 ]] && printf " Installed version $(python3 -m pip show pip | grep ^Version | awk -F ': ' '{print $NF}')\n" || exit 1
 	fi
 	if [ "x$(which ansible 2>/dev/null)" == "x" ]
 	then
-		printf "\nInstalling ansible on localhost ..."
+		printf "\nInstalling Ansible on localhost ..."
 		[[ "$(echo ${PROXY_ADDRESS} | grep ':.*@' &>/dev/null;echo ${?})" -eq 0 ]] && debug=1 && [[ "${SECON}" == "true" ]] && set +x
 		python3 -m pip install --user --no-cache-dir --quiet -I ansible==${ANSIBLE_VERSION} --proxy="${PROXY_ADDRESS}"
 		[[ ${?} == 0 ]] && printf " Installed version ${ANSIBLE_VERSION}\n" || exit 1
@@ -230,7 +224,7 @@ function install_packages() {
 			python3 -m pip uninstall -y ansible
 			find ~/.local -maxdepth 4 -name "ansible*" -exec rm -rf {} \;
 			[[ "$(echo ${PROXY_ADDRESS} | grep ':.*@' &>/dev/null;echo ${?})" -eq 0 ]] && debug=1 && [[ "${SECON}" == "true" ]] && set +x
-			printf "\nInstalling Ansible ..."
+			printf "\nInstalling Ansible on localhost ..."
 			python3 -m pip install --user --no-cache-dir --quiet -I ansible==${ANSIBLE_VERSION} --proxy="${PROXY_ADDRESS}"
 			[[ ${?} == 0 ]] && printf " Installed version ${ANSIBLE_VERSION}\n" || exit 1
 			[[ ${debug} == 1 ]] && debug=0 && set -x
