afc1df93321e6a73ca794d2e832d021771ac283f
Wassim Tout <watout@cisco.com>
2024-04-16 16:44:37 -0400

    Updated the bash directory scripts to ensure the user gets prompted to enter credentials when needed and to improve the docker login check. Also updated the "git config" commands to be compatible with Ubuntu Updated the vm_fromiso role to add "--file" option to the "git config" command

--- Bash/get_common_vault_pass.sh
+++ Bash/get_common_vault_pass.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 if [[ "$(pwd | grep -i 'cdra')" == "" ]]
 then
-	git config remote.origin.url | awk -F '/' '{print $NF}'
+	git config --file .git/config remote.origin.url | awk -F '/' '{print $NF}'
 else
 	echo "cms-build-auto-deploy.git"
 fi
