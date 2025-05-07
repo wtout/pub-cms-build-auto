7c2c1243f4473e708d1ca624fd5b2c42ab5995aa
Wassim Tout <>
2025-05-07 16:42:23 +0000

    Replaced the release.yml workflow with two new workflows:   ci.yml workflow to run tests anytime there is a commit to the develop, feature/*, and hotfix/* branches. It also merges the feature and hotfix branches to their corresponding destination branch   rba.yml workflow to run tests anytime there is a commit to the release/* branch. It also merges the release branch to both master and develop branches Updated the Bash and yaml code to comply with the coding best practices outlined in shellcheck and ansible-lint Updated the get_secrets_vault function to default localbranch to develop when dealing with feature or release branches Updated the get_secrets_vault function to default localbranch to master when dealing with a hotfix branch Release Complete

--- Bash/get_common_vault_pass.sh
+++ Bash/get_common_vault_pass.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-if [[ "$(pwd | grep -i 'cdra')" == "" ]]
+if [[ $(pwd | grep -i 'cdra') == "" ]]
 then
 	password=$(git config --file .git/config remote.origin.url | awk -F '/' '{print $NF}' | sed -e "s|^pub-||")
-	[[ "${password}" != *".git" ]] && password="${password}.git"
+	[[ ${password} != *".git" ]] && password="${password}.git"
 else
 	password="cms-build-auto.git"
 fi
